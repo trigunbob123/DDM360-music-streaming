50f23fda0247d6fb533c5500f2200e73d18d3543
enen <trigunbob123@gmail.com>
2025-07-21 16:36:56 +0800

    創建簡單的 Node.js Express 後端

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,4 +9,23 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        configure: (proxy, options) => {
+          proxy.on('error', (err, req, res) => {
+            console.log('Proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            console.log('Sending Request:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, res) => {
+            console.log('Received Response:', proxyRes.statusCode, req.url);
+          });
+        },
+      }
+    }
+  }
 })
